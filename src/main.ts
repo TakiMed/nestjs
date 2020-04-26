9074c3e771544861b6b517c76bee4c46d570d403
tatjanamedan <tatjanamedan@gmail.com>
2020-04-26 16:47:31 +0200

    validation filer

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,27 @@
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, ValidationError } from '@nestjs/common';
 import { FallbackExceptionFilter } from './poruducts/filters/fallback.filter';
 import { HttpExceptionFilter } from './poruducts/filters/http.filter';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationFilter } from './poruducts/filters/validation.filter';
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(
     new FallbackExceptionFilter(),
-    new HttpExceptionFilter()); //order from generic to more specific-vazno
+    new HttpExceptionFilter(),
+    new ValidationFilter()); //order from generic to more specific-vazno
   
   app.useGlobalPipes(new ValidationPipe({
-    skipMissingProperties:true
+    skipMissingProperties:true,
+    exceptionFactory:(errors:ValidationError[])=>{
+      const messages=errors.map(
+        error=>`${error.property} has wrong value${error.value},
+        ${Object.values(error.constraints).join(', ')}`
+      )
+
+    }
   }));
   await app.listen(3000);
 }
