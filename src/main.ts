854f7f1c99aa58da620e099bb7a30ef1bef2979c
tatjanamedan <tatjanamedan@gmail.com>
2020-04-26 15:36:12 +0200

    FallbackExceptionFilter

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { FallbackExceptionFilter } from './poruducts/filters/fallback.filter';
 import { HttpExceptionFilter } from './poruducts/filters/http.filter';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -5,7 +6,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalFilters(
+    new FallbackExceptionFilter(),
+    new HttpExceptionFilter()); //order from generic to more specific-vazno
   await app.listen(3000);
 }
 bootstrap();
